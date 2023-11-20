c64d57f7fc4757392202e6b2fa224604d6dce958
kiraashley24 <kiraselucky@gmail.com>
2023-11-20 16:46:46 -0700

    made it prettier!

--- polls/forms.py
+++ polls/forms.py
@@ -2,7 +2,11 @@ from django import forms
 from .models import Ticket
 
 class TicketForm(forms.ModelForm):
-    quantity = forms.IntegerField(min_value=1)  # Add this line
+    # Define choices for the quantity field
+    QUANTITY_CHOICES = [(str(i), str(i)) for i in range(1, 11)]  # 1 to 10
+
+    # Use ChoiceField for the quantity field
+    quantity = forms.ChoiceField(choices=QUANTITY_CHOICES, label='Quantity')
 
     class Meta:
         model = Ticket
