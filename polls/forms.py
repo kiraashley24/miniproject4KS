c46ee93105647965d029eb45794d644fce725449
kiraashley24 <kiraselucky@gmail.com>
2023-11-20 16:41:55 -0700

    makes sure quantity is 1 or more...gives error otherwise

--- polls/forms.py
+++ polls/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Ticket
 
 class TicketForm(forms.ModelForm):
+    quantity = forms.IntegerField(min_value=1)  # Add this line
+
     class Meta:
         model = Ticket
         fields = ['type', 'quantity']
